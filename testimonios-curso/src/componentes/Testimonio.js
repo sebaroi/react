3ddda24c480f5319953102b0e650ce7c20cb68f4
sebaroi <sroizenberg@gmail.com>
2022-09-12 15:50:28 -0300

    proyecto testimonios 2

---
+++ testimonios-curso/src/componentes/Testimonio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(){
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+      className='imagen-testimonio' 
+      src={require('../imagenes/testimonio-auricualres4.webp')} 
+      alt='fotos'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Emma bostios en suecia</p>
+        <p className='cargo-testimonio'>Ingenieria de software en spotify</p>
+        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+      </div>
+
+    </div>
+  ); 
+}   
+
+export default Testimonio;
