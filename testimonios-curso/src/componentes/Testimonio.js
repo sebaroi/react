e87fa0409034c7e81942d68078a7c1ade956173e
sebasroi <sroizenberg@gmail.com>
2022-09-13 07:36:24 -0300

    agregando fotos

--- testimonios-curso/src/componentes/Testimonio.js
+++ testimonios-curso/src/componentes/Testimonio.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
   return (
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio' 
-      src={require('../imagenes/testimonio-auricualres4.webp')} 
+      src={require(`../imagenes/testimonio-${props.imagen}.webp`)} 
       alt='fotos'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma bostios en suecia</p>
-        <p className='cargo-testimonio'>Ingenieria de software en spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
 
     </div>
